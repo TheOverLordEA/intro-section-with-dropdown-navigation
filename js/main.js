286a5c446ac2fd86270e1bdcb8f35cfc3250b33a
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-12-02 04:55:41 -0800

    Javascript functions created

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+function mobileMenu() {
+  document.querySelector(".burger-menu").style.display = "none";
+  document.querySelector(".nav-collapsed").style.display = "flex";
+  document.querySelector("body").style.background = "blue";
+}
 
+document.querySelector(".burger-menu").addEventListener("click", mobileMenu);
