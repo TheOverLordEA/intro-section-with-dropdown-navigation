a4a63966666575b10891af167d8fd768fce7b8a9
Erkhembayar-Atarbold <erhembold@gmail.com>
2022-12-06 07:08:44 -0800

    Nav close function built

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ function mobileMenu() {
   document.querySelector("body").style.background = "blue";
 }
 
+function closeMenu() {
+  document.querySelector(".nav-collapsed").style.display = "none";
+  document.querySelector("body").style.background = "white";
+}
+
 document.querySelector(".burger-menu").addEventListener("click", mobileMenu);
+
+document.querySelector(".nav-close").addEventListener("click", closeMenu);
